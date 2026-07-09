1b408817c5da0c8c9f18abd9e16f0d201df063ed
Emirhan Peçe <kaaner.emirhan@gmail.com>
2026-07-09 13:24:57 +0300

    add escape sequences

---
+++ EscapeCharacters/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EscapeCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metinsel ifade içinde \"Çift tırnak\" kullanımı örneğidir.");
+            Console.WriteLine("Metinsel ifade içinde \'Tek tırnak\' kullanımı örneğidir.");
+            Console.WriteLine("Metinsel ifade içinde \\Ters eğik çizgi\\ kullanımı örneğidir.");
+            Console.WriteLine("Paragrafın birinci satırıdır.\nParagrafın ikinci satırıdır.");
+            Console.WriteLine("İmleci satır başına getirir.\r:D");
+            Console.WriteLine("Bir \ttab boşluk bırakır.");
+            Console.WriteLine("Bu birinci \bmetinsel ifadedir.");
+            Console.WriteLine("Bu ikinci \vmetinsel ifadedir.");
+            Console.WriteLine("Bu bir uyarı \asesidir");
+            Console.WriteLine("Metinsel ifadeyi sonlandırır ya da null karakteri ekler: \0");
+
+            /* Ek olarak string başında '@' kullanarak kaçış karakterlerini
+               devre dışı bırakabiliriz.
+            */
+        }
+    }
+}
